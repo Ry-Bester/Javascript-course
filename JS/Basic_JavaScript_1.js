2fdfe5f1394c9a7ef1ba6126683b1d9d71743867
Ryan <zelda0169@gmailcom>
2020-08-05 17:29:54 -0600

    first JS assignment
    this is the first assignment where im using java it includes basics java and my first function

---
+++ JS/Basic_JavaScript_1.js
@@ -0,0 +1,21 @@
+
+document.write("this is a string" + " and a concatenation");
+
+var B = "Power" + " Rangers"
+document.write(B)
+
+var C = " Cat", D = "Dog", E = "Elephant";
+
+document.write(E);
+
+var red = "I am seeing Red.";
+var red = red.fontcolor("red");
+document.write(red);
+
+document.write(5 * 5);
+
+function My_First_function () {
+    var str = "this text is green";
+    var result = str.fontcolor ("Green");
+    document.getElementById("Green_Text").innerHTML= result;
+}
