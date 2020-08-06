6ae7739b678e8aac6c914d40f9396928d33736e5
Ryan <zelda0169@gmailcom>
2020-08-06 17:20:33 -0600

    corrected somehting i missed
    added an alert and  code comments

--- JS/Basic_JavaScript_1.js
+++ JS/Basic_JavaScript_1.js
@@ -1,21 +1,23 @@
 
 document.write("this is a string" + " and a concatenation");
 
-var B = "Power" + " Rangers"
+var B = "Power" + " Rangers" // this section concatinates code
 document.write(B)
 
-var C = " Cat", D = "Dog", E = "Elephant";
+var C = " Cat", D = "Dog", E = "Elephant"; //this is assigning multipl ids in one string
 
-document.write(E);
+document.write(E); //this tells the doc to print the e element
 
-var red = "I am seeing Red.";
+var red = "I am seeing Red."; // this section of code makes a sentence appear in red text
 var red = red.fontcolor("red");
-document.write(red);
+document.write(red); //
 
-document.write(5 * 5);
+document.write(5 * 5);// this tells it to display 5 multiplie dby 5
 
 function My_First_function () { // defining a function  and naming it
     var str = "this text is green"; // defing a variable and giving it a string value
     var result = str.fontcolor ("Green");// using the fontcolor method on str variable
     document.getElementById("Green_Text").innerHTML= result;// putting the value of result into html elemnt with green_text id
 }
+
+window.alert("Hello World!") ;
---
+++ JS/Basic_JavaScript_3.js
@@ -0,0 +1,23 @@
+function myFunction() {
+    var add = 2 + 2;
+    document.getElementById("Math"). innerHTML= "2 + 2 = " + add;
+}
+
+function myFunction2() {
+    var sub = 5 - 2;
+    document.getElementById("Math2"). innerHTML= "5 - 2 = " + sub;
+}
+
+function myFunction3() {
+    var mul = 5 * 2;
+    document.getElementById("Math3"). innerHTML= "5 * 2 = " + mul;
+}
+function myFunction4() {
+    var div = 10 / 2;
+    document.getElementById("Math4"). innerHTML= "10 / 2 = " + div;
+}
+
+function myFunction5() {
+    var simple_math = (5 + 10) * 6 / 3;
+    document.getElementById("Math5"). innerHTML= "five plus 10 times 6 ,divided by 3 = " + simple_math;
+}
