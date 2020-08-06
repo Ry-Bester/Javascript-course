a15a31e7d43cfff96a1890e2044c9729b0408c5f
Ryan <zelda0169@gmailcom>
2020-08-05 18:24:00 -0600

    JS submission 2
    this has other functions including concatenation

--- JS/Basic_JavaScript_2.js
+++ JS/Basic_JavaScript_2.js
@@ -1,21 +1,16 @@
-
-document.write("this is a string" + " and a concatenation");
-
-var B = "Power" + " Rangers"
-document.write(B)
-
-var C = " Cat", D = "Dog", E = "Elephant";
-
-document.write(E);
-
-var red = "I am seeing Red.";
-var red = red.fontcolor("red");
-document.write(red);
-
-document.write(5 * 5);
-
 function My_First_function () { // defining a function  and naming it
     var str = "this text is green"; // defing a variable and giving it a string value
     var result = str.fontcolor ("Green");// using the fontcolor method on str variable
     document.getElementById("Green_Text").innerHTML= result;// putting the value of result into html elemnt with green_text id
+    
+}
+
+function My_Second_function () {
+    document.getElementById("Change").innerHTML= "This changed too see???";
+}
+
+function myFunction () {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
 }
